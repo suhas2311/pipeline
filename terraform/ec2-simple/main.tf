e4fd87d20e9513f618f10e535574dde2c5c74af8
Harsha Jain <jaintpharsha@gmail.com>
2022-09-30 10:28:35 +0530

    taint, workspace, data sources 30-September-22

--- terraform/ec2-simple/main.tf
+++ terraform/ec2-simple/main.tf
@@ -10,3 +10,49 @@ module "dynamodb_locking_create" {
    hash_key = "LockID"
    bill_mode = "PAY_PER_REQUEST"
 }
+
+data "aws_ami" "get_k8s_master_ami" {
+    most_recent = true
+    owners = ["639218562896"]
+    tags = {
+      Name = "k8s"
+    }
+
+    filter {
+      name = "state"
+      values = ["available"]
+    }
+
+    filter {
+      name = "tag:Name"
+      values = ["k8s"]
+    }
+}
+
+module "create_k8s_master_node" {
+    source = "./modules/ec2_instance"
+    k8s_ami = data.aws_ami.get_k8s_master_ami.image_id
+    k8s_instance_type = var.module_k8s_instance_type
+}
+
+
+# provider "" {
+  
+# }
+
+# data "" "name" {
+  
+# }
+
+# resource "" "name" {
+  
+# }
+
+# variable "" {
+  
+# }
+
+# output "name" {
+  
+# }
+
---
+++ terraform/ec2-simple/modules/ec2_instance/varibales.tf
@@ -0,0 +1,11 @@
+variable "k8s_ami" {
+   default = ""
+}
+
+variable "k8s_instance_type" {
+   default = ""
+}
+
+# variable "k8s_name" {
+#    default = ""
+# }
